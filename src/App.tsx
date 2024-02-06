1bfb1a49341395d5b804fecfb2964dfda0e2e469
Clara <clarapachecodev@gmail.com>
2024-02-06 15:57:01 -0300

    Add `mx-auto` to centralize the container, `max-w-6xl` to define a max-width` and `my-12` to define margin top and bottom of 48px

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import logo from "./assets/logo-nlw-expert.svg";
 
 export const App = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-6xl my-12">
       <img src={logo} alt="nlw-experts-logo" />;
       <input type="text" placeholder="Busque em suas notas..." />
     </div>
