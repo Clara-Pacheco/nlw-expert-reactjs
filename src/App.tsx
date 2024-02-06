3fdcd65b8e928acd0fe1021a101053832075b71e
Clara <clarapachecodev@gmail.com>
2024-02-06 16:11:22 -0300

    Correct alternative text from image element, move input elementinside of a form element and add stylization for form element and input

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import logo from "./assets/logo-nlw-expert.svg";
 export const App = () => {
   return (
     <div className="mx-auto max-w-6xl my-12">
-      <img src={logo} alt="nlw-experts-logo" />;
-      <input type="text" placeholder="Busque em suas notas..." />
+      <img src={logo} alt="nlw-expert-logo" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
     </div>
   );
 };
