a091f72c86e2d188f5fcf30d4673e48572647101
Clara <clarapachecodev@gmail.com>
2024-02-06 15:23:17 -0300

    Add `text-slate-50` and `antialised` to the body element and add the external links for the font `Inter`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export const App: React.FC = () => {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
