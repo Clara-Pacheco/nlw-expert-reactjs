248d27a03ef4ee4ab245bdb16e44b42572e9c9c8
Clara <clarapachecodev@gmail.com>
2024-02-07 20:20:26 -0300

    Create a state to change to a textarea on the modal when the user click on the option to type a note via text

--- src/NewNoteCard.tsx
+++ src/NewNoteCard.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+import { useState } from "react";
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
+  }
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
@@ -26,17 +32,24 @@ export function NewNoteCard() {
               <span className="text-sm font-medium text-slate-300">
                 Adicionar nota
               </span>
-              <p className="text-sm leading-6 text-slate-400">
-                Comece{" "}
-                <button className="font-medium text-lime-400 hover:underline">
-                  gravando uma nota
-                </button>{" "}
-                em áudio ou se preferir{" "}
-                <button className="font-medium text-lime-400 hover:underline">
-                  utilize apenas texto
-                </button>
-                .
-              </p>
+              {shouldShowOnboarding ? (
+                <p className="text-sm leading-6 text-slate-400">
+                  Comece{" "}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    gravando uma nota
+                  </button>{" "}
+                  em áudio ou se preferir{" "}
+                  <button
+                    onClick={handleStartEditor}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    utilize apenas texto
+                  </button>
+                  .
+                </p>
+              ) : (
+                <p>Editor</p>
+              )}
             </div>
             <button
               type="button"
