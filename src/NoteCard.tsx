bd580ff5ccae43b2b71601684b4842b16f6e4004
Clara <clarapachecodev@gmail.com>
2024-02-07 16:22:54 -0300

    Add a `Dialog.Root` surrounding the return of the componenet NoteCard, change the element button for `Dialog.Trigger` -  the modal will be triggered when we click in this element, and add `Dialog.Content`, which is what will appear when the modal will show up

--- src/NoteCard.tsx
+++ src/NoteCard.tsx
@@ -7,13 +7,17 @@ interface NoteCardProps {
 
 export function NoteCard(props: NoteCardProps) {
   return (
-    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">
-        {props.date.toISOString()}
-      </span>
-      <p className="text-sm leading-6 text-slate-400">{props.content}</p>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {props.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{props.content}</p>
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+      </Dialog.Trigger>
+
+      <Dialog.Content>Oi</Dialog.Content>
+    </Dialog.Root>
   );
 }
