7ab61624d0366b4ab3c2daf0d07389df556026ec
Clara <clarapachecodev@gmail.com>
2024-02-07 15:52:41 -0300

    Create an interface for NoteCardProps and use the props instead of static values in the component

--- src/NoteCard.tsx
+++ src/NoteCard.tsx
@@ -1,12 +1,15 @@
-export function NoteCard() {
+interface NoteCardProps {
+  date: Date;
+  content: string;
+}
+
+export function NoteCard(props: NoteCardProps) {
   return (
     <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        O Drizzle possui um plugin do ESLint para evitar que realizemos updates
-        ou deletes sem where... Para configurar o plugin, é preciso instalar
-        como abaixo:
-      </p>
+      <span className="text-sm font-medium text-slate-300">
+        {props.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">{props.content}</p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
     </button>
