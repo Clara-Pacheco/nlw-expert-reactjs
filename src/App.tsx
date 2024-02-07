3e0dc912b82a9670bc97422535b5a6e4c3c6c902
Clara <clarapachecodev@gmail.com>
2024-02-07 15:55:28 -0300

    Pass the values for props in the call of the component NoteCard in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,7 @@ export const App = () => {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard date={new Date()} content="Hello World" />
       </div>
     </div>
   );
