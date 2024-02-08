4199a686c1fd8aba716ac287587004df7c4eabbb
Clara <clarapachecodev@gmail.com>
2024-02-08 16:23:02 -0300

    Create a function `handleStopRecording`, which will be triggered by a click event on the recording buttton and change the event to save a new note to a click event that will happen on the button when isRecoding is equal to false

--- src/NewNoteCard.tsx
+++ src/NewNoteCard.tsx
@@ -37,6 +37,10 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   function handleStartRecording() {
     setIsRecording(true);
   }
+
+  function handleStopRecording() {
+    setIsRecording(false);
+  }
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
@@ -59,7 +63,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               <X className="size-5" />
             </Dialog.Close>
 
-            <form onSubmit={handleSaveNote} className="flex flex-1 flex-col">
+            <form className="flex flex-1 flex-col">
               <div className="flex flex-1 flex-col gap-3 p-5">
                 <span className="text-sm font-medium text-slate-300">
                   Adicionar nota
@@ -96,14 +100,16 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
               {isRecording ? (
                 <button
-                  type="submit"
+                  type="button"
+                  onClick={handleStopRecording}
                   className="w-full bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100"
                 >
                   Gravando!(Clique para interromper)
                 </button>
               ) : (
                 <button
-                  type="submit"
+                  type="button"
+                  onClick={handleSaveNote}
                   className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
                 >
                   Salvar nota
