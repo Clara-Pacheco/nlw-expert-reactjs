0901a2225a650b33c900a4088f67547c1c25730f
Clara <clarapachecodev@gmail.com>
2024-02-08 17:12:31 -0300

    Define maximum alternatives to suggest possible words to 1

--- src/NewNoteCard.tsx
+++ src/NewNoteCard.tsx
@@ -56,6 +56,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
     speechRecognition.lang = "pt-BR";
     speechRecognition.continuous = true;
+    speechRecognition.maxAlternatives = 1;
   }
 
   function handleStopRecording() {
